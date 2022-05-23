84f21b40142658cdc5fb353feec7eeb7d9e40fb0
Fahim_Ruddro <fahim15-13570@diu.edu.bd>
2022-05-23 18:01:58 +0600

    first commit

---
+++ 1st class/1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+
+printf("The class was easy to understand");
+return 0;
+
+}
---
+++ 1st class/2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=30,b=50;
+    int calculation=(((a+b)/8)*8);
+    printf("%d\n",calculation);
+    return 0;
+}
